c8d7b0ba26456279e032ba9c80b5d554227938dd
shbrezman <shbrezman@gmail.com>
2021-07-08 19:10:24 +0300

    mongo atlas commit

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const mogoose = require('mongoose');
 const cryptModel = require('./models/cryptModel');
 const encryption = require('./utils/encryption')
 const port = 8080;
-const dbPath = 'mongodb://127.0.0.1:27017/internDB';
+
+const dbPath = 'mongodb+srv://internsDB:412336195@internscluster.lujbp.mongodb.net/internsDB?retryWrites=true&w=majority';
 
 
 
@@ -16,6 +17,7 @@ mogoose.connect(dbPath, { useNewUrlParser: true , useUnifiedTopology: true});
 
 
 app.use("/auth", require('./routes/authRoute'));
+
 
 app.use("/api", function(req, res, next){
     var userToken = new cryptModel(false, req.headers['x-access-token']);
